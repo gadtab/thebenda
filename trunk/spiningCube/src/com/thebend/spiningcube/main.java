d358807a21335d849d7153dc024e8510f387f9cf
orbenda83@gmail.com <orbenda83@gmail.com@4502a102-7499-483c-c762-ebd65f4035c6>
2011-04-30 08:29:50 +0000

    Last Edited By Or.

--- trunk/spiningCube/src/com/thebend/spiningcube/main.java
+++ trunk/spiningCube/src/com/thebend/spiningcube/main.java
@@ -1,6 +1,7 @@
 package com.thebend.spiningcube;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class main extends Activity {
@@ -8,6 +9,33 @@ public class main extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        setContentView(R.layout.app_intro);
+        
+        Thread introTimer = new Thread()
+        {
+        	public void run()
+        	{
+        		try
+        		{
+        			int time = 0;
+        			while (time < 5000)
+        			{
+        				sleep(100);
+        				time += 100;
+        			}
+        			startActivity(new Intent("com.thebend.spiningcube.STARTSCREEN"));
+        		} catch (InterruptedException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+        		finally
+        		{
+        			finish();
+        		}
+        	}
+        	
+        };
+        
+        introTimer.start();
+    }    
 }
